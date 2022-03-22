196dfb486debebc1c8e64bccce693ea9cc92c5a1
DavidRivasM <77422284+DavidRivasM@users.noreply.github.com>
2022-03-22 07:34:41 -0600

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,66 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable indent */
+/* eslint-disable semi */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import Image from 'next/image';
+
+
+const Footer = () => {
+    return (
+        <footer className="container grid place-items-center">
+            <span className="cursor-pointer font-bold text-2xl text-white">Made by David Rivas. All Rights Reserved 2022.
+            </span>
+
+
+            <ul className="flex m-8 items-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                <a className="m-4" href="mailto:alonsorivasmora@gmail.com" target="_blank" rel="noreferrer noopener">
+                    <Image
+                        unoptimized
+                        alt=""
+                        height="30px"
+                        width="30px"
+                        className="align-middle rounded-full transform transition duration-500 hover:scale-110"
+                        src="gmail1.gif"
+                    />
+                </a>
+                <a className="m-4" href="https://github.com/DavidRivasM" target="_blank" rel="noreferrer noopener">
+                    <Image
+                        unoptimized
+                        alt=""
+                        margin="5px"
+                        height="30px"
+                        width="30px"
+                        className="align-middle rounded-full transform transition duration-500 hover:scale-110"
+                        src="git.gif"
+                    />
+                </a>
+                <a className="m-4" href="https://wa.me/50685255893" target="_blank" rel="noreferrer noopener">
+                    <Image
+                        unoptimized
+                        alt=""
+                        height="30px"
+                        width="30px"
+                        className="align-middle rounded-full transform transition duration-500 hover:scale-110"
+                        src="what.gif"
+                    />
+                </a>
+                <a className="m-4" href="https://personal-portfolio-xi-taupe.vercel.app/" target="_blank" rel="noreferrer noopener">
+                    <Image
+                        unoptimized
+                        alt=""
+                        height="30px"
+                        width="30px"
+                        className="align-middle rounded-full transform transition duration-500 hover:scale-110"
+                        src="icon.gif"
+                    />
+                </a>
+            </ul>
+        </footer>
+
+    )
+}
+
+export default Footer
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { grpahCMSImageLoader } from '../util';
 
 const PostCard = ({ post }) => (
-  <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+  <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8 transform transition duration-500 hover:scale-105">
     {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
       <Image
         unoptimized
@@ -18,10 +18,10 @@ const PostCard = ({ post }) => (
       />
     </div> */}
     <div className="relative overflow-hidden shadow-md pb-80 mb-6">
-      <img src={post.featuredImage.url} alt="" className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
+      <img src={post.featuredImage.url} alt="" className="object-top absolute h-80 w-full shadow-lg rounded-t-lg lg:rounded-lg" />
     </div>
 
-    <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+    <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-red-900 text-3xl font-semibold">
       <Link href={`/post/${post.slug}`}>{post.title}</Link>
     </h1>
     <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
@@ -49,7 +49,7 @@ const PostCard = ({ post }) => (
     </p>
     <div className="text-center">
       <Link href={`/post/${post.slug}`}>
-        <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
+        <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-red-900 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
       </Link>
     </div>
   </div>
